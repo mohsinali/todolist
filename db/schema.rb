af48d5f54b25997ec4ee4257302d8f624488a31f
Mohsin <mohammadmohsin@gmail.com>
2019-01-19 13:53:36 +0500

    Generated todo model and migration. Defined associations between user and todo.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_19_082713) do
+ActiveRecord::Schema.define(version: 2019_01_19_084732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "todos", force: :cascade do |t|
+    t.text "title"
+    t.boolean "is_completed", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
