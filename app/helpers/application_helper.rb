c72965429c23079b3d44f0b1552b5f7f73e2d644
Mohsin <mohammadmohsin@gmail.com>
2019-01-19 15:07:23 +0500

    Using gem 'jwt' to authenticate apis via Json Web Token

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def generate_token user
+    payload = { data: {user: {id: user.id, email: user.email, first_name: user.first_name}} }
+    payload[:exp] = (Time.now + 10.days).to_i
+    JWT.encode payload, ENV["HMAC_SECRET"], 'HS256'
+  end
 end
